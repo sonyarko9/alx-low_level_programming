d75a238bef73ce031fa3527861e48a074b74829f
sonyarko9 <samuelokyere544@gmail.com>
2023-09-25 17:41:54 -0700

    new

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,13 @@
 /**
 * _memset - main function of the prototype
 *
-*@b: The value used to fill the memory
+* @b: The value used to fill the memory
 *
 * @s: Function paramater
 *
 * @n: Function parameter
 *
-*Return: s
+* Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
